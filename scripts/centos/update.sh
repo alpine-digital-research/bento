8ad1d313daf85c8383ac71260d7afd5038ca2490
Israel Shirk <israelshirk@gmail.com>
2016-11-07 15:45:47 -0700

    Exclude kernel updates after initial upgrade

--- scripts/centos/update.sh
+++ scripts/centos/update.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -eux
 
 yum -y update
+
+# Automatic updates to the kernel will cause vmware's tools to fall
+# out of currency, so we just preclude that behavior here.
+echo 'exclude=kernel*' >> /etc/yum.conf
+
 reboot
 sleep 60
