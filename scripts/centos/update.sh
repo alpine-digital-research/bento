5e3b5079ba6d5fa4edb24ee73c544f71a8bc6cc4
Israel Shirk <israelshirk@gmail.com>
2016-11-10 11:43:48 -0700

    Add kernel-devel for vmware...

--- scripts/centos/update.sh
+++ scripts/centos/update.sh
@@ -1,10 +1,13 @@
 #!/bin/sh -eux
 
+# Totally wrong place for this, but needed by vmware tools to successfully install.
+yum -y install kernel-devel kernel-headers
+
 yum -y update
 
 # Automatic updates to the kernel will cause vmware's tools to fall
 # out of currency, so we just preclude that behavior here.
-echo 'exclude=kernel*' >> /etc/yum.conf
+echo 'exclude=kernel' >> /etc/yum.conf
 
 reboot
 sleep 60
