101ae74e7cdcfc55b8b0013431a8f3a8888ef9ca
Israel Shirk <israelshirk@gmail.com>
2016-11-13 18:41:43 -0700

    Add perl-file-temp for vmware tools on fedora 24

--- scripts/fedora/build-tools.sh
+++ scripts/fedora/build-tools.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -eux
 # Installing build tools here because Fedora 22+ will not do so during kickstart
-dnf -y install kernel-headers-$(uname -r) kernel-devel-$(uname -r) gcc make perl perl-file-temp
+dnf -y install kernel-headers-$(uname -r) kernel-devel-$(uname -r) gcc make perl
+
+dnf -y install perl-File-Temp || echo "nope"
+dnf -y install perl-File-Tempdir || echo "nope"
