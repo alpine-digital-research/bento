7428db43211c0d23ca052c8a1a8b5fa72f3aaef4
Israel Shirk <israelshirk@gmail.com>
2016-12-08 15:48:42 -0700

    Stuff for Packer cloud

--- scripts/common/virtualbox.sh
+++ scripts/common/virtualbox.sh
@@ -11,8 +11,8 @@ virtualbox-iso|virtualbox-ovf)
     sh /tmp/vbox/VBoxLinuxAdditions.run \
         || echo "VBoxLinuxAdditions.run exited $? and is suppressed." \
             "For more read https://www.virtualbox.org/ticket/12479";
-    umount /tmp/vbox;
-    rm -rf /tmp/vbox;
-    rm -f $HOME_DIR/*.iso;
+    # umount /tmp/vbox;
+    # rm -rf /tmp/vbox;
+    # rm -f $HOME_DIR/*.iso;
     ;;
 esac
